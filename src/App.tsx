813424eac18f774229288839856d8b9be1a6633b
VirajCommits <virajmurab567@yahoo.com>
2025-07-15 14:25:57 -0400

    Remove unused useEffect import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Desktop from './components/Desktop'
 import type { WindowState } from './types'
